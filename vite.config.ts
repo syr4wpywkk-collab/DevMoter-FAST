ad9234a628d8adc74a68d7d34c8b475335ea247d
DevMoter <syr4wpywkk@privaterelay.appleid.com>
2026-09-22 03:26:18 +0900

    chore: migrate vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  server: {
+    host: "127.0.0.1",
+    port: 5173,
+    proxy: {
+      "/api": "http://127.0.0.1:8787"
+    }
+  },
+  build: {
+    target: "es2022"
+  }
+});
